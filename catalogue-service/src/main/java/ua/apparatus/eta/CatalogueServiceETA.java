f740021cfbce7f8be3e7aea25d2409962818aac8
LisovichOleksandr <lisovyhs@gmail.com>
2024-12-10 07:11:24 +0200

    application skeleton start

---
+++ catalogue-service/src/main/java/ua/apparatus/eta/CatalogueServiceETA.java
@@ -0,0 +1,12 @@
+package ua.apparatus.eta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogueServiceETA {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogueServiceETA.class, args);
+    }
+}
